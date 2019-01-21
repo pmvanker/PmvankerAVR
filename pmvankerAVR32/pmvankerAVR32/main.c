7fc15f8ba6189d99ab877cb8522c6756b715f809
Praful Vanker <pmvanker@gmail.com>
2019-01-21 15:07:36 +0530

    added avrstudio project

---
+++ pmvankerAVR32/pmvankerAVR32/main.c
@@ -0,0 +1,18 @@
+/*
+ * pmvankerAVR32.c
+ *
+ * Created: 21-01-2019 15:03:02
+ * Author : pmvan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
